8dc9ce0fc186f7d30017a31d97d9061e51d03e65
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-11-04 09:48:41 +0900

    ルーティング修正。Homeを書き換え

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
+import Form from '@/components/Form'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Form',
+      component: Form
     }
   ]
 })

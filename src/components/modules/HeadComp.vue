16ce2b57cac7cfe300b57f234c3062bbf8eae77f
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-11-05 16:26:18 +0900

    storeに値を設定

--- src/components/modules/HeadComp.vue
+++ src/components/modules/HeadComp.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'headComp',
-  data () {
-    return {
-      title: '感想を入力'
-    }
-  }
+  computed: mapGetters({
+    'title': 'getTitle'
+  })
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -11,12 +11,36 @@ const Form = {
   actions: {
     buttonAction({ commit, state, rootState }) {
       console.log("buttonAction")
+      commit('setStepCount', null, {root: true}) //rootへのアクセス
+    }
+  }
+}
+
+const Head = {
+  state: {
+    title: ["感想を入力", "確認画面", "送信完了"]
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    getTitle (state, getters, rootState) {
+      return state.title[rootState.stepCount]
     }
   }
 }
 
 export default new Vuex.Store({
+  state: {
+    stepCount: 0
+  },
+  mutations: {
+    setStepCount (state) {
+      console.log("rootsetStepCount")
+      state.stepCount++
+    }
+  },
   modules: {
-    Form
+    Form,
+    Head
   }
 })
